b601a15592471a1afd191e8ff5aa3b09d318fc78
Uvindu-bit <upathirana112@gmail.com>
2021-02-23 14:34:50 +0530

    testcommittwo

---
+++ src/app/models/product.ts
@@ -0,0 +1,9 @@
+export class Product {
+  id: number;
+  namer: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+
+
+}
---
+++ src/app/service/product.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  products = [
+
+  ]
+
+  constructor() { }
+}
+// test

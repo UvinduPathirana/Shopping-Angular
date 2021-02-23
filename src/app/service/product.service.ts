190bd6a3bd2813874cb61d3197cce6a5a7a21d68
Uvindu-bit <upathirana112@gmail.com>
2021-02-23 14:58:10 +0530

    product.service.ts update

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-
+import { Product } from '../models/product'
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  products = [
-
+  products: Product[] = [
+    new Product(1, 'Product 1', 'This is the product 1 description. The product is really cool!', 100),
   ]
 
   constructor() { }

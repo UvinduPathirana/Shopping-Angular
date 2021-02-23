2cc344a3ccc4721b8f98f467a7ff0ead8735c13a
Uvindu-bit <upathirana112@gmail.com>
2021-02-23 14:51:05 +0530

    products.ts update

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -5,5 +5,11 @@ export class Product {
   price: number;
   imageUrl: string;
 
-
+  constructor(id, name, description, price, imageUrl){
+    this.id = id
+    this.name = name
+    this.description = description
+    this.price = price
+    this.imageUrl = imageUrl
+  }
 }

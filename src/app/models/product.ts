7c9a6e38fc9e950e90ce26383025907aacf9f1df
Uvindu-bit <upathirana112@gmail.com>
2021-02-23 15:01:36 +0530

    products.ts update

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,11 +1,11 @@
 export class Product {
   id: number;
-  namer: string;
+  name: string;
   description: string;
   price: number;
   imageUrl: string;
 
-  constructor(id, name, description = '', price = '', imageUrl = 'https://www.sun-sentinel.com/resizer/57GyO6vyw_s7714FuO9l_WK_lcM=/630x630/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/XYVLGQ5LKVBH5FIPRS5PFYK4J4.jpg'){
+  constructor(id, name, description = '', price, imageUrl = 'https://www.sun-sentinel.com/resizer/57GyO6vyw_s7714FuO9l_WK_lcM=/630x630/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/XYVLGQ5LKVBH5FIPRS5PFYK4J4.jpg'){
     this.id = id
     this.name = name
     this.description = description
--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -6,6 +6,11 @@ import { Product } from '../models/product'
 export class ProductService {
   products: Product[] = [
     new Product(1, 'Product 1', 'This is the product 1 description. The product is really cool!', 100),
+    new Product(2, 'Product 2', 'This is the product 2 description. The product is really cool!', 560),
+    new Product(3, 'Product 3', 'This is the product 3 description. The product is really cool!', 260),
+    new Product(4, 'Product 4', 'This is the product 4 description. The product is really cool!', 300),
+    new Product(5, 'Product 5', 'This is the product 5 description. The product is really cool!', 100),
+    new Product(6, 'Product 6', 'This is the product 6 description. The product is really cool!', 400),
   ]
 
   constructor() { }

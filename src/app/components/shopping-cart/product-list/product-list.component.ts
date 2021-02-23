1868be1f8a19d9e72fbd9641c9989b20f69e40b6
Uvindu-bit <upathirana112@gmail.com>
2021-02-23 15:14:06 +0530

    prodcut list update

--- src/app/components/shopping-cart/product-list/product-list.component.ts
+++ src/app/components/shopping-cart/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProductService } from 'src/app/service/product.service'
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    console.log(this.productService.getProducts())
   }
 
 }
